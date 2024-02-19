83c73d116fd4c4f843dbad067976525ff8b493f9
Martin Lopez <tincho1597lopez@gmail.com>
2024-02-19 19:17:14 -0300

    algo

--- Gestion_de_Bibliotecav3/Controladores/Controlador Libro.cs
+++ Gestion_de_Bibliotecav3/Controladores/Controlador Libro.cs
@@ -14,12 +14,30 @@ namespace Gestion_de_Bibliotecav3.Controladores
 
         public Task<List<Libro>> BuscarEjemplaresPorIsbnONombre(string isbnONombre)
         {
-            return servicioLibro.BuscarLibrosPorIsbnONombre(isbnONombre);
+            try
+            {
+                return servicioLibro.BuscarLibrosPorIsbnONombre(isbnONombre);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
         }
 
         public List<Libro> BuscarLibroPorNombreOISBN(string nombreOISBN)
         {
-            return servicioLibro.BuscarLibroPorNombreOISBN(nombreOISBN);
+            try
+            {
+                return servicioLibro.BuscarLibroPorNombreOISBN(nombreOISBN);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
         }
     }
 }
