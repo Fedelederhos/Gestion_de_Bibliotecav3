b4984b38675c8870ae5e9278761da592e2388d20
Luciana Gabriela Valiente <54379062+lucianagabrielavaliente@users.noreply.github.com>
2024-02-22 05:27:31 -0300

    Update formularios

--- Gestion_de_Bibliotecav3/GUI/ChildForms/Categoria/GestionCategoriaForm.Designer.cs
+++ Gestion_de_Bibliotecav3/GUI/ChildForms/Categoria/GestionCategoriaForm.Designer.cs
@@ -28,14 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             panelTabla = new Panel();
             textBusqueda = new TextBox();
             buttonBuscar = new Button();
             labelIngrese = new Label();
             categoriasGrid = new DataGridView();
+            codigo = new DataGridViewTextBoxColumn();
             nombre = new DataGridViewTextBoxColumn();
             panelTabla.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)categoriasGrid).BeginInit();
@@ -94,47 +95,53 @@
             categoriasGrid.BorderStyle = BorderStyle.None;
             categoriasGrid.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
             categoriasGrid.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle4.BackColor = Color.FromArgb(30, 45, 57);
-            dataGridViewCellStyle4.Font = new Font("Microsoft Sans Serif", 7.2F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle4.ForeColor = Color.Gainsboro;
-            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            categoriasGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(30, 45, 57);
+            dataGridViewCellStyle1.Font = new Font("Microsoft Sans Serif", 7.2F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = Color.Gainsboro;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            categoriasGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             categoriasGrid.ColumnHeadersHeight = 25;
             categoriasGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            categoriasGrid.Columns.AddRange(new DataGridViewColumn[] { nombre });
+            categoriasGrid.Columns.AddRange(new DataGridViewColumn[] { codigo, nombre });
             categoriasGrid.EnableHeadersVisualStyles = false;
             categoriasGrid.GridColor = Color.FromArgb(30, 45, 57);
             categoriasGrid.Location = new Point(134, 81);
             categoriasGrid.Name = "categoriasGrid";
             categoriasGrid.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = Color.FromArgb(37, 52, 57);
-            dataGridViewCellStyle5.Font = new Font("Microsoft Sans Serif", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle5.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle5.SelectionBackColor = SystemColors.MenuHighlight;
-            dataGridViewCellStyle5.SelectionForeColor = Color.FromArgb(224, 224, 224);
-            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
-            categoriasGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(37, 52, 57);
+            dataGridViewCellStyle2.Font = new Font("Microsoft Sans Serif", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.MenuHighlight;
+            dataGridViewCellStyle2.SelectionForeColor = Color.FromArgb(224, 224, 224);
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            categoriasGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             categoriasGrid.RowHeadersVisible = false;
             categoriasGrid.RowHeadersWidth = 51;
-            dataGridViewCellStyle6.BackColor = Color.FromArgb(37, 52, 57);
-            dataGridViewCellStyle6.Font = new Font("Microsoft Sans Serif", 6.5F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle6.ForeColor = Color.Gainsboro;
-            dataGridViewCellStyle6.SelectionBackColor = SystemColors.MenuHighlight;
-            dataGridViewCellStyle6.SelectionForeColor = Color.FromArgb(224, 224, 224);
-            categoriasGrid.RowsDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(37, 52, 57);
+            dataGridViewCellStyle3.Font = new Font("Microsoft Sans Serif", 6.5F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.Gainsboro;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.MenuHighlight;
+            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(224, 224, 224);
+            categoriasGrid.RowsDefaultCellStyle = dataGridViewCellStyle3;
             categoriasGrid.RowTemplate.Height = 24;
             categoriasGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             categoriasGrid.Size = new Size(539, 358);
             categoriasGrid.TabIndex = 0;
             // 
+            // codigo
+            // 
+            codigo.HeaderText = "Código";
+            codigo.MinimumWidth = 6;
+            codigo.Name = "codigo";
+            codigo.Width = 70;
+            // 
             // nombre
             // 
             nombre.HeaderText = "Nombre";
-            nombre.MinimumWidth = 6;
             nombre.Name = "nombre";
             nombre.Width = 73;
             // 
@@ -158,6 +165,7 @@
         private System.Windows.Forms.Button buttonBuscar;
         private System.Windows.Forms.Label labelIngrese;
         private System.Windows.Forms.TextBox textBusqueda;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private DataGridViewTextBoxColumn codigo;
+        private DataGridViewTextBoxColumn nombre;
     }
 }
